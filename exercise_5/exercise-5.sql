cd7f179a2e84a3533efe86dc66d51d964a38c713
Violet Stewart <vkazoo2@gmail.com>
2022-03-24 15:46:16 -0700

    tests four and five pass

--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,8 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+SELECT
+    title
+FROM
+    film
+LEFT JOIN
+    
